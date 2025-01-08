306ecf9d568426bfc716fdc310771c0d88e36a33
Emma Luk <emma07315@gmail.com>
2025-01-08 11:39:22 -0700

    baseurl changed
    fix for expo go to backend network. Need ip address for url depending on device

--- src/frontend/context/api.tsx
+++ src/frontend/context/api.tsx
@@ -9,52 +9,55 @@ import { useAuth } from "@/context/auth";
 
 // API utility function
 const api = (token: string | null = null) => {
-  const baseUrl = "http://127.0.0.1:8000";
+    // local host url
+    //   const baseUrl = "http://127.0.0.1:8000";
+    // emma's url
+    const baseUrl = 'http://10.18.76.54:8000';
 
-  const getAuthHeaders = () => {
-    
-    const headers: Record<string, string> = {
-      "Content-Type": "application/json",
+    const getAuthHeaders = () => {
+
+        const headers: Record<string, string> = {
+            "Content-Type": "application/json",
+        };
+        if (token) {
+            headers["Authorization"] = `Bearer ${token}`;
+        }
+        return headers;
     };
-    if (token) {
-      headers["Authorization"] = `Bearer ${token}`;
-    }
-    return headers;
-  };
 
-  // GET request method with logging
-  const get = async (url: string) => {
-    const headers = getAuthHeaders();
-    console.log("GET Request URL:", baseUrl + url);
-    console.log("GET Headers:", headers);
+    // GET request method with logging
+    const get = async (url: string) => {
+        const headers = getAuthHeaders();
+        console.log("GET Request URL:", baseUrl + url);
+        console.log("GET Headers:", headers);
 
-    return fetch(baseUrl + url, {
-      method: "GET",
-      headers,
-    });
-  };
+        return fetch(baseUrl + url, {
+            method: "GET",
+            headers,
+        });
+    };
 
-  // POST request method with logging
-  const post = async (url: string, body: Record<string, unknown>) => {
-    const headers = getAuthHeaders();
-    console.log("POST Request URL:", baseUrl + url);
-    console.log("POST Body:", JSON.stringify(body));
-    console.log("POST Headers:", headers);
+    // POST request method with logging
+    const post = async (url: string, body: Record<string, unknown>) => {
+        const headers = getAuthHeaders();
+        console.log("POST Request URL:", baseUrl + url);
+        console.log("POST Body:", JSON.stringify(body));
+        console.log("POST Headers:", headers);
 
-    return fetch(baseUrl + url, {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers,
-    });
-  };
+        return fetch(baseUrl + url, {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers,
+        });
+    };
 
-  return { get, post };
+    return { get, post };
 };
 
 // Custom hook to provide the API instance with the current token
 const useApi = () => {
-  const { token } = useAuth();
-  return api(token);
+    const { token } = useAuth();
+    return api(token);
 };
 
 export { api, useApi };
