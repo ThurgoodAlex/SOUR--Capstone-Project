9c8277a72e44104c6646ec65137b7ff8d5de6b29
ThurgoodAlex <adthurgood@gmail.com>
2025-01-13 10:32:54 -0700

    Finished basic routes for images. get_Image_by_user is not tested because posts is not set up

--- src/backend/lambda/image_handlers.py
+++ src/backend/lambda/image_handlers.py
@@ -17,3 +17,61 @@ def upload_image_lambda(event, context):
             "statusCode": 500,
             "body": json.dumps({"error": str(e)})
         }
+    
+def get_all_images_lambda(event, context):
+    try:
+        image_data = json.loads(event['body'])  
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "User created successfully",
+                "user": image_data
+            })
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
+    
+
+def get_image_by_id_lambda(event, context):
+    try:
+        image_data = json.loads(event['body'])  
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "User created successfully",
+                "user": image_data
+            })
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
+    
+
+
+def get_image_by_user_lambda(event, context):
+    try:
+        image_data = json.loads(event['body'])  
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "User created successfully",
+                "user": image_data
+            })
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
+    
