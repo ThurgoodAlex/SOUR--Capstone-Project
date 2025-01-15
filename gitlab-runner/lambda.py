71b4ecbbdcc1ac9ff635fe828f82998aee9c6226
Ricardo Baeza <ricardobaeza@dhcp-10-18-134-192.uconnect.utah.edu>
2025-01-15 13:23:23 -0700

    improve pipelines to start containers when we want to run a job, stop containers when pipeline has finished

--- gitlab-runner/lambda.py
+++ gitlab-runner/lambda.py
@@ -3,28 +3,41 @@ import boto3
 import os
 
 def lambda_handler(event, context):
+    event = json.loads(event['body'])
 
-    body = json.loads(event['body'])
-    object_kind = body["object_kind"]
-    
+    event_type = event['event_type']
+
     ec2 = boto3.client('ec2')
-    instance_id = os.environ['RUNNER_INSTANCE_ID']
-    
-    if object_kind == "push":
-        response = ec2.start_instances(
-            InstanceIds=[instance_id]
-        )
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Started GitLab runner instance')
-        }
-    elif object_kind == "pipeline":
 
-        
-        response = ec2.stop_instances(
-            InstanceIds=[instance_id]
-        )
+    instance_id = os.environ['RUNNER_INSTANCE_ID']
+
+    response = ec2.describe_instances(
+        InstanceIds=[instance_id]
+    ) 
+
+    current_state = response['Reservations'][0]['Instances'][0]['State']['Name']
+    print(f"Current instance state: {current_state}")
+
+    if event_type == "push":
+        if current_state == 'stopped':
+            response = ec2.start_instances(InstanceIds=[instance_id])
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Started GitLab runner instance')
+            }
+        else:
+            return {
+                'statusCode': 200,
+                'body': json.dumps(f'Instance is already {current_state}')
+            }
+    elif event_type == "cleanup"
+        ec2.stop_instances(InstanceIds=[instance_id])
         return {
-            'statusCode': 200,
-            'body': json.dumps('Stopped GitLab runner instance')
+            'statusCode' : 200, 
+            'body': json.dumps('Gitlab runner stopped, resources cleaned up')
+        }
+    else: 
+        return { 
+            'statusCode': 400, 
+            'body': json.dumps('malformed request') 
         }
