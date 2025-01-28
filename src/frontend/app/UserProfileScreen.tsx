398798b1f2c956d1a27e2a3392e3cbb89c2360c3
amhansen2 <ashlynhansen33@gmail.com>
2025-01-28 09:09:49 -0700

    Show user's posts on profile screen

--- src/frontend/app/UserProfileScreen.tsx
+++ src/frontend/app/UserProfileScreen.tsx
@@ -1,35 +1,98 @@
 import { useState, useEffect } from 'react';
-import { View, Text, Image, Alert, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, Image, Alert, StyleSheet, ScrollView, TouchableOpacity, ImageBackground, FlatList } from 'react-native';
 import { ScreenStyles, Styles, TextStyles } from '@/constants/Styles';
 
-import { Stack } from 'expo-router';
+import { router, Stack } from 'expo-router';
 import { useApi } from '@/context/api';
 import { NavBar } from '@/components/NavBar';
 import { StatsBar } from '@/components/StatsBar';
-import { User } from '@/constants/Types';
+import { Post, User } from '@/constants/Types';
 import { useSearchParams } from 'expo-router/build/hooks';
+import { Tabs } from '@/components/Tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function UserProfileScreen() {
     const searchParams = useSearchParams(); // Retrieve query parameters
     const userParam = searchParams.get('user');
     const api = useApi();
     const user: User | null = userParam ? JSON.parse(userParam) : null; 
-    const [listings, setListings] = useState([]);
+    const [posts, setPosts] = useState<Post[]>([]);
 
+    const dummyImages = [
+            require('../assets/images/video.png'),
+            require('../assets/images/post.png'),
+            require('../assets/images/sweater1.png'),
+            require('../assets/images/listing2.png'),
+            require('../assets/images/listing.png'),
+            require('../assets/images/random1.png'),
+            require('../assets/images/random2.png'),
+            require('../assets/images/random3.png'),
+            require('../assets/images/random4.png'),
+            require('../assets/images/random5.png'),
+        ];
+        
+        const fetchPosts = async () =>
+        {
+        try {
+            let endpoint = `/users/${user?.id}/posts/`;
+            const response = await api.get(endpoint);
+            const result = await response.json();
+            console.log(result)
+        
+            if (response.ok) {
+                const getRandomImage = () =>
+                    dummyImages[Math.floor(Math.random() * dummyImages.length)];
+            
+                const transformedPosts: Post[] = await Promise.all(
+                    result.map(async (item: any) => {
+                    return {
+                        id: item.id,
+                        createdDate: item.created_at || new Date().toISOString(),
+                        coverImage: getRandomImage(),
+                        title: item.title,
+                        description: item.description,
+                        brand: item.brand,
+                        condition: item.condition,
+                        size: item.size,
+                        gender: item.gender,
+                        price: item.price,
+                        isSold: item.isSold,
+                        isListing: item.isListing,
+                        seller: user
+                    };
+                    })
+                );
+            
+                console.log(`Received posts from ${endpoint}:`, transformedPosts);
+                setPosts(transformedPosts);
+            } else {
+            console.log(response);
+            throw new Error('Could not fetch posts.');
+            }
+        } catch (error) {
+            console.error('Error fetching posts:', error);
+            throw new Error('Failed to connect to the server.');
+        }
+        }
     
+        // Fetch listings on page load
+        useEffect(() => {fetchPosts(); }, []);
+       
 
-    return (
-        <>
-            <Stack.Screen options={{ title: 'UserProfileScreen' }} />
-            <View style={ScreenStyles.screen}>
-                <ProfileInfo user={user} />
-                <StatsBar user={user} />
-                <PostsGrid listings={listings} />
-            </View>
-            <NavBar />
-        </>
-    );
+        return (
+            <>
+                <Stack.Screen options={{ title: 'UserProfileScreen' }} />
+                <View style={ScreenStyles.screen}>
+                    <ProfileInfo user={user} />
+                    <StatsBar user={user} />
+                    <PostsGrid posts={posts} />
+                </View>
+                <NavBar />
+            </>
+        );
 }
+
+
 
 function ProfileInfo({ user }: { user: User | null }) {
     return (
@@ -44,22 +107,59 @@ function ProfileInfo({ user }: { user: User | null }) {
     );
 }
 
-function PostsGrid({ listings }: { listings: any[] }) {
-    return (
-        <ScrollView contentContainerStyle={{ padding: 10 }}>
-            {listings.length > 0 ? (
-                listings.map((item) => (
-                    <View key={item.id} style={UserProfileStyles.listingItem}>
-                        <Text style={TextStyles.h2}>{item.title}</Text>
-                        <Text style={TextStyles.p}>{item.description}</Text>
-                        <Text style={TextStyles.p}>${parseFloat(item.price).toFixed(2)}</Text>
-                    </View>
-                ))
-            ) : (
-                <Text>No posts yet!</Text>
-            )}
-        </ScrollView>
+
+
+function PostPreview({ post}: { post: Post }){
+    let icon;
+    let type = post.isListing ? "listing" : "post";
+
+    // if (type === 'listing') {
+    //     icon = <Ionicons size={20} name='videocam' />
+    // }
+    if (type === 'post') {
+        icon = <Ionicons size={20} name='megaphone' />
+    }
+    else if (type === 'listing') {
+        icon = <Ionicons size={20} name='pricetag' />
+    }
+
+    return( 
+        <View key={post.id} style={[Styles.column, { marginBottom: 1 }]}>
+            <TouchableOpacity
+                onPress={() => router.push(`/ListingInfoScreen/${post.id}`)} // Navigate on press
+                style={{ flex: 1, margin: 5 }} // Add styles for spacing
+            >
+                <ImageBackground source={post.coverImage} style={{ height: 150, width: 150 }}>
+                    {icon}
+                </ImageBackground>
+                <Text style={[TextStyles.h3, {textAlign:'left'}]}>{post.title}</Text>
+
+            </TouchableOpacity>
+        </View>
     );
+}
+
+
+
+function PostsGrid({ posts }: { posts: Post[] }) {
+    
+    
+    const renderPost = ({ item }: {item: Post}) => (
+        <PostPreview
+          post={item}
+        />
+    );
+
+   return ( 
+        <FlatList
+            data={posts} // Data for FlatList
+            keyExtractor={(item) => item.id.toString()} // Unique key for each item
+            renderItem={renderPost} // Function to render each item
+            numColumns={2} // Grid layout with 2 columns
+            columnWrapperStyle={Styles.grid} // Style for the row container
+            showsVerticalScrollIndicator={false}
+        />
+    )
 }
 
 const UserProfileStyles = StyleSheet.create({
