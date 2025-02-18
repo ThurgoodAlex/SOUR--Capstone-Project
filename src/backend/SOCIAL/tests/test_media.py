98c7f89c309d7f219ab19f42b54269f11d7abced
ricardobaeza <baezaricardo@proton.me>
2025-02-18 01:57:13 +0000

    include new route that accepts a file and post_id and updates the postgres database unit test that verifies that sending an image alongside a post id stores the image in an s3 bucket and updates the postgres db with it.

--- src/backend/SOCIAL/tests/test_media.py
+++ src/backend/SOCIAL/tests/test_media.py
@@ -11,6 +11,9 @@ from TestUtilites.TestEnvironmentManager import TestEnvManager
 from pathlib import Path
 from passlib.context import CryptContext
 from sqlalchemy import text 
+import python_multipart
+***REMOVED***
+from sqlalchemy import Engine
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -33,36 +36,68 @@ def env_manager(session: Session, client: TestClient):
     manager = TestEnvManager(session, client, data)
     return manager
 
+    
 def test_media_file_upload(client:TestClient, engine, env_manager:TestEnvManager):
 
     arthur = data[0]
     request_headers = env_manager.get_auth_headers(arthur.username, "Boah1899")
 
-    env_manager.create_post(arthur.username, "my profile pic")
+    post_id = env_manager.create_post(arthur.username, "my profile pic")
 
     current_dir = Path(__file__).parent
 
     test_image = current_dir / 'assets' / 'arthur.png'
 
     with open(test_image, 'rb') as image_file:
-        image = {
-            'file': (test_image.name, image_file, 'image/png')
+        
+        file = {
+            'file': ('arthur.png', image_file, 'image/png')
         }
-        response = client.post("/media/upload", headers=request_headers, files=image)
+        form_data = {
+            'post_id' : post_id
+        }
+
+        response = client.post("/media/upload", headers=request_headers,files=file, data = form_data)
 
     assert response.status_code == 201 
 
+    response_data = response.json()
+
+    response_data_id = response_data["id"]
+
     with engine.connect() as conn:
-        file_upload = conn.execute(text("SELECT * FROM 'media' WHERE postID = 1")).first()
+        file_upload = conn.execute(text(f'SELECT * FROM media WHERE id = {response_data_id}')).first()
 
-    media_upload = MediaInDB(**file_upload._mapping)
+    assert response_data["isVideo"] == False
+    
+    url_regex_pattern = r'http://localstack:4566/sour-user-images-000000000000-us-west-1/1_\d{8}_\d{6}_arthur\.png'
 
-    assert media_upload.url == 'http://localstack:4566/sour-user-images-000000000000-us-west-1/1_20250214_204155_arthur.png'
-    assert media_upload.isVideo == False
+    assert re.match(url_regex_pattern, response_data["url"]) is not None
+    assert re.match(url_regex_pattern, file_upload.url) is not None
+
+@pytest.fixture()
+def create_media_in_db(client:TestClient, engine:Engine, env_manager:TestEnvManager):
+    arthur = data[0]
+    request_headers = env_manager.get_auth_headers(arthur.username, "Boah1899")
+
+    post_id = env_manager.create_post(arthur.username, "my profile pic")
+
+    current_dir = Path(__file__).parent
+
+    test_image = current_dir / 'assets' / 'arthur.png'
+
+    with open(test_image, 'rb') as image_file:
+        
+        file = {
+            'file': ('arthur.png', image_file, 'image/png')
+        }
+        form_data = {
+            'post_id' : post_id
+        }
+
+        response = client.post("/media/upload", headers=request_headers,files=file, data = form_data)
 
 
-
-    print(response)
-
-
-
+@pytest.mark.skip(reason="Test has not been implemented")
+def test_obtaining_url_from_s3():
+    pass
