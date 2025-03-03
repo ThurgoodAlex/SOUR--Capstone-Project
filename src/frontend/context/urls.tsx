29327946865503adea7aa270508192c418fc2b55
amhansen2 <ashlynhansen33@gmail.com>
2025-03-03 18:10:16 +0000

    typo

--- src/frontend/context/urls.tsx
+++ src/frontend/context/urls.tsx
@@ -1,14 +1,14 @@
  //tunnel URL
- const localhostIP = "127.0.0.1";
+ //const localhostIP = "127.0.0.1";
 
  //Alex desktop URL
- //const baseUrl = "10.0.0.62";
+ //const localhostIP = "10.0.0.62";
 
- //Ashlyn's URL (please don't delete me)
- //const baseUrl = "10.18.224.228";
+ //Ashlyn's URL 
+ const localhostIP = "10.18.224.228";
 
  // emma's url
- //const baseUrl = '10.18.75.111';
+ //const localhostIP = '10.18.75.111';
 
     const fullUrl = "http://" + localhostIP + ":8000";
 
